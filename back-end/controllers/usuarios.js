cfeb5931a69edec0b13af3c2afc6bb621a9d92d8
Lucas Rassilan Vilanova <lucasrsv1@gmail.com>
2022-02-06 21:24:00 -0300

    Correção rotas usuário

--- back-end/controllers/usuarios.js
+++ back-end/controllers/usuarios.js
@@ -1,117 +1,68 @@
-const { body, param } = require("express-validator");
+const { body } = require("express-validator");
+const { sha512 } = require("js-sha512");
 
 const db = require("../database");
-const { ensureAuthorizedEmployee } = require("./login");
+const { ensureAuthorized } = require("./login");
 const { isRequestInvalid } = require("../utils/http-validation");
 
 /**
  * @param {import("express").Request} req
  * @param {import("express").Response} res
  */
-async function getAll(req, res) {
-    try {
-        const dados = await db.findAll(`
-			SELECT id_usuario,nome,email,funcionario
-			FROM usuarios
+async function insert (req, res) {
+	if (isRequestInvalid(req, res)) return;
+
+	try {
+		// Faz o hash da senha antes de fazer o login
+		const password = sha512(req.body.senha);
+
+		const result = await db.execute(`
+			INSERT INTO usuarios (nome, email, senha, funcionario)
+			VALUES ('${req.body.nome}', '${req.body.email}', '${password}', false)
+			RETURNING id_usuario AS "idUsuario", email, funcionario
 		`);
 
-        const map = [];
-        for (const dado of dados) {
-            map.push({
-                id: dado.id_usuario,
-                email: dado.email,
-                nome: dado.nome,
-                funcionario: dado.funcionario
-            });
-        }
-        res.status(200).json(map);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: error.message, error });
-    }
-}
-
-/**
- * @param {import("express").Request} req
- * @param {import("express").Response} res
- */
-async function insert(req, res) {
-    if (isRequestInvalid(req, res)) return;
-    var user = req.body;
-    try {
-        const result = await db.execute(`
-			INSERT INTO usuarios (nome,email,senha,funcionario)
-			VALUES ('${user.nome}','${user.email}','${user.senha}','${user.funcionario}')
-			RETURNING id_usuario AS "idUsuario"
-		`);
-        console.log(result)
-        res.status(201).json(result.rows[0]);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: error.message, error });
-    }
+		res.status(201).json(result.rows[0]);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: error.message, error });
+	}
 }
 
 insert.validations = [
-    body("nome").isString().withMessage("Usuario inválido.")
+	body("nome").isString().withMessage("O nome do usuário é inválido."),
+	body("email").isString().withMessage("O e-mail do usuário é inválido."),
+	body("senha").isString().withMessage("A senha do usuário é inválido.")
 ];
 
 /**
  * @param {import("express").Request} req
  * @param {import("express").Response} res
  */
-async function update(req, res) {
-    if (isRequestInvalid(req, res)) return;
+async function update (req, res) {
+	if (isRequestInvalid(req, res)) return;
 
-    try {
-        const result = await db.execute(`
+	try {
+		const result = await db.execute(`
 			UPDATE usuarios SET nome = '${req.body.nome}'
 			WHERE id_usuario = ${req.body.idUsuario}
 			RETURNING id_usuario AS "idUsuario", nome
 		`);
 
-        if (result.rowCount > 0)
-            res.status(200).json(result.rows[0]);
-        else
-            res.status(404).json({ message: "Usuario não encontrado." });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: error.message, error });
-    }
+		if (result.rowCount > 0)
+			res.status(200).json(result.rows[0]);
+		else
+			res.status(404).json({ message: "Usuario não encontrado." });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: error.message, error });
+	}
 }
 
 update.validations = [
-    ensureAuthorizedEmployee,
-    body("nome").isString().withMessage("Nome inválido."),
-    body("idUsuario").isNumeric().withMessage("A ID do usuario é inválida.").toInt()
+	ensureAuthorized,
+	body("nome").isString().withMessage("Nome inválido."),
+	body("idUsuario").isNumeric().withMessage("A ID do usuario é inválida.").toInt()
 ];
 
-/**
- * @param {import("express").Request} req
- * @param {import("express").Response} res
- */
-async function remove(req, res) {
-    if (isRequestInvalid(req, res)) return;
-console.log(req.params.idUsuario)
-    try {
-        const result = await db.execute(`
-			DELETE FROM usuarios
-			WHERE id_usuario = ${req.params.idUsuario}
-		`);
-
-        if (result.rowCount > 0)
-            res.status(200).json(result.rowCount);
-        else
-            res.status(404).json({ message: "Usuario não encontrado." });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: error.message, error });
-    }
-}
-
-remove.validations = [
-    ensureAuthorizedEmployee,
-    param("idUsuario").isNumeric().withMessage("A ID do usuario é inválida.").toInt()
-];
-
-module.exports = { getAll, insert, update, remove };
+module.exports = { insert, update };
--- back-end/routes/v1/usuarios.js
+++ back-end/routes/v1/usuarios.js
@@ -1,16 +1,11 @@
 const { Router } = require("express");
 
 const usuariosController = require("../../controllers/usuarios");
-const { ensureAuthorizedEmployee } = require("../../controllers/login");
 
 const router = Router();
-
-router.get("/",ensureAuthorizedEmployee, usuariosController.getAll);
 
 router.post("/", usuariosController.insert.validations, usuariosController.insert);
 
 router.put("/", usuariosController.update.validations, usuariosController.update);
-
-router.delete("/:idUsuario", usuariosController.remove.validations, usuariosController.remove);
 
 module.exports = router;
